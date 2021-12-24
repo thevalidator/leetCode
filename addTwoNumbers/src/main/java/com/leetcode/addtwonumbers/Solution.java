dfa86e3c393578ffccbb110fba9b57b1a25aa34b
thevalidator <the.validator@yandex.ru>
2021-12-24 17:43:56 +0300

    small fixes

--- addTwoNumbers/src/main/java/com/leetcode/addtwonumbers/Solution.java
+++ addTwoNumbers/src/main/java/com/leetcode/addtwonumbers/Solution.java
@@ -10,28 +10,27 @@ public class Solution {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        int overload = 0;
+        int carry = 0;
         int firstNodeValue = l1.val + l2.val;
         if (firstNodeValue > 9) {
-            overload = 1;
+            carry = 1;
             firstNodeValue = firstNodeValue % 10;
         }
         ListNode node = new ListNode(firstNodeValue);
         ListNode current = node;
         ListNode nextNode1 = l1.next;
         ListNode nextNode2 = l2.next;
-        
 
         while (true) {
             
+            if (nextNode1 == null && nextNode2 == null && carry == 0) {
+                break;
+            }
+
             int next1 = 0;
             int next2 = 0;
             int value = 0;
-            
-             if (nextNode1 == null && nextNode2 == null && overload == 0) {
-                break;
-            }
-            
+
             if (nextNode1 != null) {
                 next1 = nextNode1.val;
                 nextNode1 = nextNode1.next;
@@ -40,26 +39,46 @@ public class Solution {
                 next2 = nextNode2.val;
                 nextNode2 = nextNode2.next;
             }
-           
-            
-            int sum = next1 + next2 + overload;
+
+            int sum = next1 + next2 + carry;
             if (sum > 9) {
-                int number = 0;
-                if (sum != 10) {
-                    number = sum % 10;
-                }
-                value += number;
-                overload = 1;
+                value += sum % 10;
+                carry = 1;
             } else {
                 value += sum;
-                overload = 0;
+                carry = 0;
             }
-            
+
             current.next = new ListNode(value);
             current = current.next;
         }
 
         return node;
+    }
+
+    //from leetcode
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 
 }
